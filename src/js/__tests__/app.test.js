06e0ad6ab6f59b9afbcab1929a4761314f921510
Polina Filippova <polina17012001@mail.ru>
2021-07-08 14:39:12 +0300

    version2

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,8 @@
-import {
-  describe
-} from 'yargs';
-import Team from '../app';
 import Character from '../character';
+import Team from '../app';
 
 test('create player', () => {
-  const player1 = JSON.stringify(new Character('Polina', 'Bowman'));
+  const player1 = new Character('Polina', 'Bowman');
 
   expect(player1).toEqual({
     attack: 25,
@@ -17,49 +14,46 @@ test('create player', () => {
   });
 });
 
-
-describe('check methods: ', () => {
-  test('add', () => {
-    const player1 = JSON.stringify(new Character('Polina', 'Bowman'))
-    const client = new Team()
-
-    expect(client.add(player1)).toEqual(Team {
-      members: '{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}'
-    });
+test('new Team', () => {
+  expect(new Team()).toEqual({
+    members: Set {}
   });
+});
 
-  test('addAll', () => {
-    const player1 = JSON.stringify(new Character('Polina', 'Bowman'))
-    const player2 = JSON.stringify(new Character('Kate', 'Bowman'))
-    const client = new Team()
+test('add', () => {
+  const player1 = JSON.stringify(new Character('Polina', 'Bowman'));
+  const client = new Team();
 
-    expect(client.addAll([player1, player2])).toEqual(Team {
-      members: Set {
-        '{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}',
-        '{"attack":25,"defence":25,"health":100,"level":1,"name":"Kate","type":"Bowman"}'
-      }
-    });
-  });
+  expect(client.add(player1)).toEqual('{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}');
+});
 
-  test('addAll Error', () => {
-    const player1 = JSON.stringify(new Character('Polina', 'Bowman'))
-    const player2 = JSON.stringify(new Character('Kate', 'Bowman'))
-    const player3 = JSON.stringify(new Character('Kate', 'Bowman'))
+test('addAll', () => {
+  const player1 = JSON.stringify(new Character('Polina', 'Bowman'));
+  const player2 = JSON.stringify(new Character('Kate', 'Bowman'));
+  const client = new Team();
 
-    const client = new Team()
+  expect(client.addAll([player1, player2])).toEqual('{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}',
+    '{"attack":25,"defence":25,"health":100,"level":1,"name":"Kate","type":"Bowman"}');
+});
 
-    expect(client.addAll([player1, player2, player3])).toThrowError('Игрок существует');
-  });
+test('addAll Error', () => {
+  const player1 = JSON.stringify(new Character('Polina', 'Bowman'));
+  const player2 = JSON.stringify(new Character('Kate', 'Bowman'));
+  const player3 = JSON.stringify(new Character('Kate', 'Bowman'));
 
-  test('toArray', () => {
-    const player1 = JSON.stringify(new Character('Polina', 'Bowman'))
-    const player2 = JSON.stringify(new Character('Kate', 'Bowman'))
+  const client = new Team();
 
-    const client = new Team()
+  expect(client.addAll([player1, player2, player3])).toThrowError('Игрок существует');
+});
 
-    expect(client.addAll([player1, player2]).toArray()).toEqual([
-      '{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}',
-      '{"attack":25,"defence":25,"health":100,"level":1,"name":"Kate","type":"Bowman"}'
-    ]);
-  });
-})
+test('toArray', () => {
+  const player1 = JSON.stringify(new Character('Polina', 'Bowman'));
+  const player2 = JSON.stringify(new Character('Kate', 'Bowman'));
+
+  const client = new Team();
+
+  expect(client.addAll([player1, player2]).toArray()).toEqual([
+    '{"attack":25,"defence":25,"health":100,"level":1,"name":"Polina","type":"Bowman"}',
+    '{"attack":25,"defence":25,"health":100,"level":1,"name":"Kate","type":"Bowman"}',
+  ]);
+});
